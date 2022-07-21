994b63da2796683c97c0c37cb2307ec0f6af21a8
Helicobacto <yangju0411@gmail.com>
2022-07-21 11:30:34 +0900

    ver. 1.1.0
    py, sql 코드 파일로 분리
    store 릴레이션 추가
    설명 추가
    ddl 코드 추가

---
+++ applist_to_db.py
@@ -0,0 +1,23 @@
+import psycopg2
+import requests
+import json
+
+dbname = ""
+user = ""
+password = ""
+host = ""
+port = 0
+
+conn = psycopg2.connect(dbname = dbname, user= user,
+                        password= password, host= host, port= port)
+cur = conn.cursor()
+app_list_get = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+app_json = json.loads(app_list_get.text)
+
+for app in app_json["applist"]["apps"]:
+    if app["name"]:
+        try:
+            cur.execute("INSERT INTO app (appid, name) VALUES (%s, %s)", (app["appid"], app["name"]))
+        except BaseException as e:
+            print(e)
+            print(app["appid"], app["name"])
---
+++ ddl.sql
@@ -0,0 +1,91 @@
+create table app(
+    appid int primary key,
+    name varchar(65532) not null
+);
+
+create table appdetail(
+    appid int primary key,
+    header varchar(65532),
+    release_date date,
+    type varchar(50),
+    constraint fk_appid
+        foreign key(appid)
+            references app(appid)
+            on delete cascade
+);
+
+create table dlc(
+    appid int,
+    dlcid int,
+    primary key(appid, dlcid),
+    constraint fk_appid
+        foreign key(appid)
+            references appdetail(appid)
+            on delete cascade,
+    constraint fk_dlcid
+        foreign key(dlcid)
+            references app(appid)
+            on delete cascade
+);
+
+create table store(
+    store_id serial primary key,
+    store varchar(50) not null
+);
+
+create table price(
+    date date,
+    store_id int,
+    appid int,
+    price int not null,
+    init_price int not null,
+    discount int,
+    constraint fk_store_id
+        foreign key(store_id)
+            references store(store_id)
+            on delete cascade,
+    constraint fk_appid
+        foreign key(appid)
+            references appdetail(appid)
+            on delete cascade,
+    primary key(date, store_id, appid)
+);
+
+create table developer(
+    devid serial primary key,
+    name varchar(200) not null
+);
+
+create table app_dev(
+    devid int,
+    appid int,
+    constraint fk_devid
+        foreign key(devid)
+            references developer(devid)
+            on delete cascade,
+    constraint fk_appid
+        foreign key(appid)
+            references appdetail(appid)
+            on delete cascade,
+    primary key(devid, appid)
+);
+
+create table genre(
+    genid serial primary key,
+    genre varchar(50) not null
+);
+
+create table app_genre(
+    genid int,
+    appid int,
+    constraint fk_genid
+        foreign key(genid)
+            references genre(genid)
+            on delete cascade,
+    constraint fk_appid
+        foreign key(appid)
+            references appdetail(appid)
+            on delete cascade,
+    primary key(genid, appid)
+);
+
